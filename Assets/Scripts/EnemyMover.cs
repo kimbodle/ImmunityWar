de597570b4a089e8c537c759759f5e287b1bec22
kimbodle <103980156+kimbodle@users.noreply.github.com>
2024-08-09 17:30:14 +0900

    Prefab Variant, LERF

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>(); //웨이포인트의 리스트는 path. 적이 따라야하는 경로
-    [SerializeField] float waitTime = 1f;
+    [SerializeField] [Range(0f,5f)] float EnemySpeed = 1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,9 +19,18 @@ public class EnemyMover : MonoBehaviour
     {
         foreach(Waypoint waypoint in path)
         {
-            //Debug.Log(waypoint.name);
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(waitTime);
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercnet = 0f;
+            
+            transform.LookAt(endPosition);
+
+            while(travelPercnet < 1f) //1은 종료지점
+            {
+                travelPercnet+= Time.deltaTime * EnemySpeed; //매 프레임 deltaTiem이 travelPercent에 더해짐
+                transform.position = Vector3.Lerp(startPosition,endPosition, travelPercnet);
+                yield return new WaitForEndOfFrame(); //초 즉, 고정된 시간동안 기다리는 대신 프레임의 끝까지 기다린 다음 다시 코루틴 시작
+            }
         }
     }
 }
--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] bool isPlacable;
+    
+    private void OnMouseDown()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (isPlacable)
+        {
+            Debug.Log(transform.name);
+        }
     }
 }
